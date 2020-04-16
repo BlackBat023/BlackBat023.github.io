71ee8434218e27498ec0072422890779f29a8464
BlackBat023 <mjver@mweb.co.za>
2020-04-16 20:39:31 +0200

    multiple updates

--- js/angular.js
+++ js/angular.js
@@ -5,8 +5,9 @@ myBlog.controller("blogCtrl", function ($scope, $http) {
 
     scope.title = "My News";
     //API callback URI
-    var url = "https://www.googleapis.com/blogger/v3/blogs/6081790175238890686/posts?&key=AIzaSyBM8dxhhp1Zf0PFTFpLslGHYEW55Z9SRnk";
-
+    var blogId = "6081790175238890686";
+    var accessToken = "AIzaSyBM8dxhhp1Zf0PFTFpLslGHYEW55Z9SRnk";
+    var url = "https://www.googleapis.com/blogger/v3/blogs/" + blogId + "/posts?&key=" + accessToken;
 
     $http.get(url).then(function (data) {
         //variable to hold the items obj in data
@@ -15,4 +16,11 @@ myBlog.controller("blogCtrl", function ($scope, $http) {
     }).catch(function (error) {
         console.log("Error: " + error);
     });
+
+    /*
+    var url = "https://bloggerstefan-skliarovv1.p.rapidapi.com/getSinglePostById";
+    var blogId = "6081790175238890686";
+    var accessToken = "AIzaSyBM8dxhhp1Zf0PFTFpLslGHYEW55Z9SRnk";
+    */
+
 });
